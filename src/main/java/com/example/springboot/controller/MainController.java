10fdf468abb80bf82948770e63795c12e65d29a5
0ndrei <lungu.andrei.00@mail.ru>
2021-09-20 12:23:36 +0300

    Main page

--- src/main/java/com/example/springboot/controller/MainController.java
+++ src/main/java/com/example/springboot/controller/MainController.java
@@ -1,4 +1,10 @@
 package com.example.springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+    @GetMapping("/")
+    public String mainPage() {return "Main page";}
 }

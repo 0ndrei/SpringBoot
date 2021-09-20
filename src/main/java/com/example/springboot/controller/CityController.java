3a9349e44ba22173b489013369e151a84ca5d29d
0ndrei <lungu.andrei.00@mail.ru>
2021-09-20 12:31:54 +0300

    Method create

--- src/main/java/com/example/springboot/controller/CityController.java
+++ src/main/java/com/example/springboot/controller/CityController.java
@@ -1,4 +1,16 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.repository.CitysRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CityController {
+    CitysRepository citysRepository = new CitysRepository();
+
+    @PostMapping("countries")
+    public String create(@RequestParam String cityName){
+        return citysRepository.create(cityName);
+    }
 }
--- src/main/java/com/example/springboot/repository/CitysRepository.java
+++ src/main/java/com/example/springboot/repository/CitysRepository.java
@@ -1,4 +1,19 @@
 package com.example.springboot.repository;
 
+import java.util.ArrayList;
+
 public class CitysRepository {
+    ArrayList<String> city = new ArrayList<>();
+
+    public CitysRepository(){
+        this.city.add("Balti");
+        this.city.add("Chisinau");
+        this.city.add("Ocnita");
+        this.city.add("Falesti");
+    }
+
+    public String create(String countryName){
+        this.city.add(countryName);
+        return "City created";
+    }
 }
